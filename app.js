a60136774a52a0ef8b4bf62ec7220db75446f09c
BenjaminGordonCody <b.gordoncody@gmail.com>
2022-01-07 14:40:38 +0000

    can now win and lose

--- app.js
+++ app.js
@@ -22,21 +22,78 @@ let targetDetails = [
   },
 ];
 
-let play = (target, targetDetail) => {
-  target.style.background = targetDetail.highlight;
-  console.log(targetDetail.sound);
-  targetDetail.sound.play();
-  setTimeout(function () {
-    target.style.background = targetDetail.color;
-  }, 200);
-};
+function defineTargets() {
+  let lightAndSound = (target, targetDetail) => {
+    target.style.background = targetDetail.highlight;
+    targetDetail.sound.play();
+    setTimeout(function () {
+      target.style.background = targetDetail.color;
+    }, 200);
+  };
 
-//make targets in browser
-let targets = document.getElementsByClassName("target");
-for (let i = 0; i < targets.length; i++) {
-  targets[i].style.background = targetDetails[i].color;
-  targets[i].onclick = () => {
-    document.getElementById("recordOfClicks").append(i);
-    play(targets[i], targetDetails[i]);
+  //make targets in browser
+  let targets = document.getElementsByClassName("target");
+  for (let i = 0; i < targets.length; i++) {
+    targets[i].style.background = targetDetails[i].color;
+    targets[i].onclick = () => {
+      document.getElementById("recordOfClicks").append(i);
+      lightAndSound(targets[i], targetDetails[i]);
+    };
+  }
+}
+
+function generateSequence(length, limit) {
+  let sequence = [];
+  for (let i = 0; i < length; i++) {
+    sequence[i] = Math.floor(Math.random() * limit);
+  }
+  return sequence.join("");
+}
+
+function hasWon() {
+  let sequence = document.getElementById("sequence").innerText;
+  let recordOfClicks = document.getElementById("recordOfClicks").innerText;
+
+  if (sequence == recordOfClicks) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+defineTargets();
+
+function setSequence(length) {
+  let sequence = generateSequence(length, 4);
+  document.getElementById("sequence").innerText = sequence;
+}
+
+function game() {
+  let refresh = () => {
+    setSequence(5);
+    document.getElementById("recordOfClicks").innerText = "";
+  };
+
+  let gameState = "playing";
+  setSequence(5);
+
+  document.body.onclick = () => {
+    console.log(gameState);
+    if (gameState == "playing" && hasWon()) {
+      gameState = "gameEnd";
+      console.log("winner");
+    } else if (gameState == "gameEnd") {
+      refresh();
+      gameState = "playing";
+    } else if (
+      gameState == "playing" &&
+      document.getElementById("recordOfClicks").innerText.length >=
+        document.getElementById("sequence").innerText.length
+    ) {
+      gameState = "gameEnd";
+      console.log("loser");
+    }
   };
 }
+
+game();
